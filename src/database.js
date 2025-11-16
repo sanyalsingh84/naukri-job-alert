82f4191841da303f12e7ab7f57726b27031a1442
Sanyal Singh <sanyalsingh84@gmail.com>
2025-11-16 17:37:43 +0530

    new database integration

--- src/database.js
+++ src/database.js
@@ -7,12 +7,10 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("error", (err, client) => {
